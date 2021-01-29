1bcd736cebd72bae5ab4c5db03fc153ea809067b
indratdev <indrat.dev@gmail.com>
2021-01-29 22:59:17 +0700

    add Register Model

---
+++ Register Mini/Model/Register.swift
@@ -0,0 +1,17 @@
+//
+//  Register.swift
+//  Register Mini
+//
+//  Created by IndratS on 29/01/21.
+//  Copyright © 2021 IndratSaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Register {
+    let firstName: String
+    let lastName: String
+    let age: Int
+    let sex: String
+    let address: String
+}
