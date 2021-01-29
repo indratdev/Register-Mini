5d02de5c1a47f776726004be07a0a8fac5a8e3d1
indratdev <indrat.dev@gmail.com>
2021-01-29 22:29:26 +0700

    Initial Commit

---
+++ Register Mini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Register Mini
+//
+//  Created by IndratS on 29/01/21.
+//  Copyright © 2021 IndratSaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
